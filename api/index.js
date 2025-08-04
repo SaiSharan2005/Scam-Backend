8b3820a930691781c14a9d2f239dafbe6a24c985
DUGINISAISHARAN <duginisaisharan@gmail.com>
2025-08-04 18:24:48 +0530

    fixed backend

--- api/index.js
+++
@@ -1,3 +0,0 @@
-// api/index.js
-const app = require("../src/index");
-module.exports = app;
--- index.js
+++ index.js
@@ -65,14 +65,11 @@ app.use("/admin",dashboardScreen);
 app.use("/admin",profile)
 
 
-const PORT = process.env.PORT || 5000;
 
-if (require.main === module) {
+// if (require.main === module) {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-}
+// }
 
 // app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
-// Export the app for Vercel
-module.exports = app;
